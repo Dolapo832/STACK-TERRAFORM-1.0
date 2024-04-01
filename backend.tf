3a04dac5f23f3a1381b0a86ba6fc371c58bad386
Dolapo832 <Dolapo832@gmail.com>
2024-03-31 22:53:26 -0400

    adding modified backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform{
          backend "s3"{
-                bucket= "stack-terraformstate-newdolaposep23"
+                bucket= "stack-terraformstate-newdolaposep11"
                 key = "terraform.tfsate"
                 region="us-east-1"
                 dynamodb_table="statelock-tf"
